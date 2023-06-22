bcbafe72c60fb9c4a0603da025d574f58df61a70
franc <franc@DESKTOP-GE8FKMJ>
2023-06-22 14:33:35 +0200

    Caro Salvatore hai perso due volte!

---
+++ ShoemustGoonN/src/control/AjaxUtente.java
@@ -0,0 +1,87 @@
+package control;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.Iterator;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import model.UtenteBean;
+import model.UtenteModelDS;
+
+
+//Servlet per l'uso di AJAX nella ricerca dei prodotti
+@WebServlet("/AjaxUtente")
+public class AjaxUtente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxUtente() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        
+        PrintWriter out = response.getWriter();
+        String oggettoJSON = null;
+        
+		System.out.println(request.getParameter("stringaRicerca"));
+		
+		UtenteModelDS Utente = new UtenteModelDS();
+		
+		try {
+			if(!request.getParameter("stringaRicerca").equalsIgnoreCase("")) {
+				Collection<UtenteBean> UtenteSuggest = Utente.doRetrieveSuggest(request.getParameter("stringaRicerca"));
+				Iterator<UtenteBean> iter = UtenteSuggest.iterator();
+				
+				UtenteBean utente = null;
+				while(iter.hasNext()) {
+					utente = iter.next();
+					System.out.println("I prodotti ricevuti dalla servlet sono: "+ utente.getID_Utente() + " " + utente.getCognome() + "" + utente.getNome());
+				}
+				
+				oggettoJSON = new Gson().toJson(UtenteSuggest);
+				System.out.println("Oggetto JSON: "+oggettoJSON);
+				
+				response.getWriter().write(oggettoJSON.toString());
+			} else {
+				oggettoJSON = new Gson().toJson("");
+				response.getWriter().write(oggettoJSON.toString());
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
